e18da84ed9929b34930012e8b7b047f59d75c470
XakHD <zcheldercraftllc@gmail.com>
2024-09-18 10:25:29 -0400

    website coming soon

--- src/components/Pricing/PricingBox.tsx
+++ src/components/Pricing/PricingBox.tsx
@@ -1,10 +1,69 @@
+// import axios from "axios";
+// import OfferList from "./OfferList";
+// import { Price } from "@/types/priceItem";
+
+
+// const PricingBox = ({ product }: { product: Price }) => {
+//   // POST request
+//   const handleSubscription = async (e: any) => {
+//     e.preventDefault();
+//     const { data } = await axios.post(
+//       "/api/payment",
+//       {
+//         priceId: product.id,
+//       },
+//       {
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       },
+//     );
+
+//     // console.log(data);
+//     window.location.assign(data);
+//   };
+
+//   return (
+//     <div className="w-full">
+//       <div className="relative z-10 rounded-sm bg-white px-8 py-10 shadow-three hover:shadow-one dark:bg-gray-dark dark:shadow-two dark:hover:shadow-gray-dark">
+//         <div className="flex items-center justify-between">
+//           <h3 className="price mb-2 text-3xl font-bold text-black dark:text-white">
+//             $
+//             <span className="amount">
+//               {(product.unit_amount / 100).toLocaleString("en-US", {
+//                 currency: "USD",
+//               })}
+//             </span>
+//             {/* <span className="time text-body-color">/{duration}</span> */}
+//           </h3>
+//           <h4 className="mb-2 text-xl font-bold text-dark dark:text-white">
+//             {product.nickname} Plan
+//           </h4>
+//         </div>
+//         <p className="mb-7 text-base text-body-color">
+//          Payments are currently not live(test mode)
+//         </p>
+//         <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
+//           <button 
+//             onClick={handleSubscription}
+//             className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
+//           >
+//             Buy Now
+//           </button>
+//           <button 
+//             // onClick={handleSubscription}
+//             className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp mt-2"
+//           >
+            
+//           </button>
+         
+//         </div>
+//         <div>
 import axios from "axios";
 import OfferList from "./OfferList";
 import { Price } from "@/types/priceItem";
-import MintNFT from "@/components/MintNFT";
 
 const PricingBox = ({ product }: { product: Price }) => {
-  // POST request
   const handleSubscription = async (e: any) => {
     e.preventDefault();
     const { data } = await axios.post(
@@ -16,10 +75,9 @@ const PricingBox = ({ product }: { product: Price }) => {
         headers: {
           "Content-Type": "application/json",
         },
-      },
+      }
     );
 
-    // console.log(data);
     window.location.assign(data);
   };
 
@@ -34,23 +92,21 @@ const PricingBox = ({ product }: { product: Price }) => {
                 currency: "USD",
               })}
             </span>
-            {/* <span className="time text-body-color">/{duration}</span> */}
           </h3>
           <h4 className="mb-2 text-xl font-bold text-dark dark:text-white">
             {product.nickname} Plan
           </h4>
         </div>
         <p className="mb-7 text-base text-body-color">
-         Payments are currently not live(test mode)
+          Payments are currently not live (test mode)
         </p>
         <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
-          <button 
+          <button
             onClick={handleSubscription}
             className="flex w-full items-center justify-center rounded-sm bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp"
           >
             Buy Now
           </button>
-         
         </div>
         <div>
           {product.nickname === "Basic" && (
